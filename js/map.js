9afe740edde46e7b9118f37e433ace1c267dc4d9
svetanto <svetanto@gmail.com>
2017-11-29 01:40:36 +0300

    Структурно переработан скрипт отрисовки меток и карточки

--- js/map.js
+++ js/map.js
@@ -1,89 +1,221 @@
 'use strict';
 
 var numberOfObjects = 8;
-var locationMinX = 300;
-var locationMaxX = 900;
-var locationMinY = 100;
-var locationMaxY = 500;
-var minPrice = 1000;
-var maxPrice = 1000000;
-var minRooms = 1;
-var maxRooms = 5;
-var maxGuests = 100;
 
-var AVATARS = generateAvatarsURLs('img/avatars/user0', '.png', numberOfObjects);
-var TITLES_VALUES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var TITLES = shuffleArray(TITLES_VALUES);
-var LOCATIONS_X = generateLocations(locationMinX, locationMaxX, numberOfObjects);
-var LOCATIONS_Y = generateLocations(locationMinY, locationMaxY, numberOfObjects);
-var ADDRESSES = generateAddresses(numberOfObjects);
-var PRICES = generatePrices(numberOfObjects);
-var TYPES = ['flat', 'house', 'bungalo'];
-var CHECKINS = ['12:00', '13:00', '14:00'];
-var CHECKOUTS = ['12:00', '13:00', '14:00'];
-var FEATURES_VALUES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var FEATURES = generateFeatures(numberOfObjects);
+var inputObject = generateInput(numberOfObjects);
+var arrayOfOfferedObjects = generateOfferedObjects(inputObject, numberOfObjects);
+drawMapPins(arrayOfOfferedObjects);
+drawMapCard(arrayOfOfferedObjects[0]);
 
-var objects = generateOfferedObjects(numberOfObjects);
+// Функция формирования входных данных (эмуляция внешнего ввода, потом будет заменена на внешний ввод)
+function generateInput(numberOfInputs) {
 
-var mapCardButtonTemplate = document.querySelector('template').content;
-var mapButtonTemplate = mapCardButtonTemplate.querySelector('.map__pin');
-var mapCardTemplate = mapCardButtonTemplate.querySelector('.map__card');
-var pinsOverlay = document.querySelector('.map__pins');
-var cardOverlay = document.querySelector('.map');
+  var AVATAR_URL_PREFIX = 'img/avatars/user';
+  var AVATAR_URL_SUFFIX = '.png';
+  var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+  var LOCATION_MIN_X = 300;
+  var LOCATION_MAX_X = 900;
+  var LOCATION_MIN_Y = 100;
+  var LOCATION_MAX_Y = 500;
+  var MIN_PRICE = 1000;
+  var MAX_PRICE = 1000000;
+  var TYPES = ['flat', 'house', 'bungalo'];
+  var MIN_ROOMS_NUMBER = 1;
+  var MAX_ROOMS_NUMBER = 5;
+  var MAX_GUESTS = 100;
+  var CHECKINS = ['12:00', '13:00', '14:00'];
+  var CHECKOUTS = ['12:00', '13:00', '14:00'];
+  var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
-drawMapPins(pinsOverlay);
-drawMapCard(cardOverlay, objects[0]);
+  var avatars = generateAvatarsURLs(AVATAR_URL_PREFIX, AVATAR_URL_SUFFIX, numberOfInputs);
+  var titles = shuffleArray(TITLES);
+  var locationsX = generateRandomArray(LOCATION_MIN_X, LOCATION_MAX_X, numberOfInputs);
+  var locationsY = generateRandomArray(LOCATION_MIN_Y, LOCATION_MAX_Y, numberOfInputs);
+  var addresses = generateAddresses();
+  var prices = generateRandomArray(MIN_PRICE, MAX_PRICE, numberOfInputs);
+  var types = generateTypes(TYPES, numberOfInputs);
+  var rooms = generateRandomArray(MIN_ROOMS_NUMBER, MAX_ROOMS_NUMBER, numberOfInputs);
+  var guests = generateRandomArray(1, MAX_GUESTS, numberOfInputs);
+  var checkins = generateRandomArrayfromInput(CHECKINS, numberOfInputs);
+  var checkouts = generateRandomArrayfromInput(CHECKOUTS, numberOfInputs);
+  var features = generateFeatures(FEATURES, numberOfInputs);
+  var descriptions = '';
+  var photos = [];
 
-function renderMapPin(pin) {
-  var mapPin = mapButtonTemplate.cloneNode(true);
-  var mapPinWidth = 40;
-  var mapPinHeight = 44;
-  mapPin.style = 'left: ' + (pin.location.x - mapPinWidth / 2) + 'px; top: ' + (pin.location.y - mapPinHeight) + 'px';
-  mapPin.querySelector('img').src = pin.author.avatar;
-  return mapPin;
+  return {
+    avatars: avatars,
+    titles: titles,
+    addresses: addresses,
+    prices: prices,
+    types: types,
+    rooms: rooms,
+    guests: guests,
+    checkins: checkins,
+    checkouts: checkouts,
+    features: features,
+    descriptions: descriptions,
+    photos: photos,
+    x: locationsX,
+    y: locationsY
+  };
+
+  // Специфичные функции формирования входных данных ===========================
+  function generateAvatarsURLs(prefix, suffix, number) {
+    var array = [];
+    var avatarURLs = [];
+    for (var i = 1; i <= number; i++) {
+      array.push((i < 10) ? '0' + i : i);
+    }
+    array = shuffleArray(array);
+    for (i = 0; i < number; i++) {
+      avatarURLs.push(prefix + array[i] + suffix);
+    }
+    return avatarURLs;
+  }
+
+  function generateAddresses() {
+    var array = [];
+    for (var i = 0; i < locationsX.length; i++) {
+      array.push(locationsX[i] + ', ' + locationsY[i]);
+    }
+    return array;
+  }
+
+  function generateTypes(estateTypes, number) {
+    var array = [];
+    var type;
+    for (var i = 0; i < number; i++) {
+      switch (estateTypes[generateRandomInteger(0, estateTypes.length - 1)]) {
+        case 'flat': type = 'Квартира';
+          break;
+        case 'house': type = 'Дом';
+          break;
+        case 'bungalo': type = 'Бунгало';
+          break;
+      }
+      array.push(type);
+    }
+    return array;
+  }
+
+  function generateFeatures(input, number) {
+    var array = [];
+    for (var i = 0; i < number; i++) {
+      var arrayTemp = [];
+      var featuresNumber = generateRandomInteger(1, input.length);
+      var featuresValuesShuffled = shuffleArray(input);
+      for (var j = 0; j < featuresNumber; j++) {
+        arrayTemp.push(featuresValuesShuffled[j]);
+      }
+      array[i] = arrayTemp;
+    }
+    return array;
+  }
 }
 
-function drawMapPins(pins) {
+// Функция формирования массива из входных данных
+function generateOfferedObjects(input, number) {
+  var array = [];
+  for (var i = 0; i < number; i++) {
+    array[i] =
+        {
+          author: generateAuthorObject(i),
+          offer: generateOfferObject(i),
+          location: generateLocationObject(i)
+        };
+  }
+  return array;
+
+  function generateAuthorObject(num) {
+    var object = {
+      avatar: input.avatars[num]
+    };
+    return object;
+  }
+
+  function generateOfferObject(num) {
+    var object = {
+      title: input.titles[num],
+      address: input.addresses[num],
+      price: input.prices[num],
+      type: input.types[num],
+      rooms: input.rooms[num],
+      guests: input.guests[num],
+      checkin: input.checkins[num],
+      checkout: input.checkouts[num],
+      features: input.features[num],
+      description: input.descriptions,
+      photos: input.photos
+    };
+    return object;
+  }
+
+  function generateLocationObject(num) {
+    var object = {
+      x: input.x[num],
+      y: input.y[num]
+    };
+    return object;
+  }
+}
+
+// Функция отрисовки меток на карте
+function drawMapPins(objects) {
+  var mapCardPinTemplate = document.querySelector('template').content;
+  var mapPinTemplate = mapCardPinTemplate.querySelector('.map__pin');
+  var pinsOverlay = document.querySelector('.map__pins');
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < objects.length; i++) {
     fragment.appendChild(renderMapPin(objects[i]));
   }
-  pins.appendChild(fragment);
-}
+  pinsOverlay.appendChild(fragment);
 
-function renderMapCard(data) {
-  var mapCard = mapCardTemplate.cloneNode(true);
-  mapCard.querySelector('h3').textContent = data.offer.title;
-  mapCard.querySelector('small').textContent = data.offer.address;
-  mapCard.querySelector('.popup__price').textContent = data.offer.price + ' ¥/ночь';
-  // Линтер ругается на вложенный тернарный оператор - что - ифами что ли делать эту проверку??
-  // Или какой-то вспомогательный массив создавать? Не понимаю - почему бы сразу в массив не записать правильные строки...
-  mapCard.querySelector('h4').textContent = (data.offer.type === 'flat') ? 'Квартира' : (data.offer.type === 'house') ? 'Дом' : 'Бунгало';
-  mapCard.querySelector('p:nth-of-type(3)').textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests + ' гостей';
-  mapCard.querySelector('p:nth-of-type(4)').textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
-
-  var popupFeatures = mapCard.querySelector('.popup__features');
-  var blocks = popupFeatures.querySelectorAll('li');
-  for (var i = 0; i < popupFeatures.children.length; i++) {
-    if (data.offer.features[i]) {
-      blocks[i].classList = 'feature feature--' + data.offer.features[i];
-    } else {
-      popupFeatures.removeChild(blocks[i]);
-    }
+  function renderMapPin(pin) {
+    var mapPin = mapPinTemplate.cloneNode(true);
+    var mapPinWidth = 40;
+    var mapPinHeight = 44;
+    mapPin.style = 'left: ' + (pin.location.x - mapPinWidth / 2) + 'px; top: ' + (pin.location.y - mapPinHeight) + 'px';
+    mapPin.querySelector('img').src = pin.author.avatar;
+    return mapPin;
   }
-
-  mapCard.querySelector('p:nth-of-type(5)').textContent = data.offer.description;
-  mapCard.querySelector('.popup__avatar').src = data.author.avatar;
-  return mapCard;
 }
 
-function drawMapCard(card, data) {
+// Функция отрисовки карточки объекта
+function drawMapCard(objects) {
+  var mapCardPinTemplate = document.querySelector('template').content;
+  var mapCardTemplate = mapCardPinTemplate.querySelector('.map__card');
+  var cardOverlay = document.querySelector('.map');
   var fragment = document.createDocumentFragment();
-  fragment.appendChild(renderMapCard(data));
-  card.appendChild(fragment);
-  card.insertBefore(fragment, card.querySelector('.map__filters-container'));
+  fragment.appendChild(renderMapCard(objects));
+  cardOverlay.appendChild(fragment);
+  cardOverlay.insertBefore(fragment, cardOverlay.querySelector('.map__filters-container'));
+
+  function renderMapCard(data) {
+    var mapCard = mapCardTemplate.cloneNode(true);
+    mapCard.querySelector('h3').textContent = data.offer.title;
+    mapCard.querySelector('small').textContent = data.offer.address;
+    mapCard.querySelector('.popup__price').textContent = data.offer.price + ' ₽/ночь';
+    mapCard.querySelector('h4').textContent = data.offer.type;
+    mapCard.querySelector('p:nth-of-type(3)').textContent = data.offer.rooms + ' комнаты для ' + data.offer.guests + ' гостей';
+    mapCard.querySelector('p:nth-of-type(4)').textContent = 'Заезд после ' + data.offer.checkin + ', выезд до ' + data.offer.checkout;
+
+    var popupFeatures = mapCard.querySelector('.popup__features');
+    var blocks = popupFeatures.querySelectorAll('li');
+    for (var i = 0; i < blocks.length; i++) {
+      if (data.offer.features[i]) {
+        blocks[i].classList = 'feature feature--' + data.offer.features[i];
+      } else {
+        popupFeatures.removeChild(blocks[i]);
+      }
+    }
+
+    mapCard.querySelector('p:nth-of-type(5)').textContent = data.offer.description;
+    mapCard.querySelector('.popup__avatar').src = data.author.avatar;
+    return mapCard;
+  }
 }
+
+// Утилитарные функции =========================================================
 
 function shuffleArray(array) {
   for (var i = array.length - 1; i; i--) {
@@ -99,20 +231,7 @@ function generateRandomInteger(fromNumber, toNumber) {
   return Math.round(Math.random() * (toNumber - fromNumber) + fromNumber);
 }
 
-function generateAvatarsURLs(prefix, suffix, number) {
-  var array = [];
-  var avatarURLs = [];
-  for (var i = 0; i < number; i++) {
-    array.push(i + 1);
-  }
-  array = shuffleArray(array);
-  for (i = 0; i < number; i++) {
-    avatarURLs.push(prefix + array[i] + suffix);
-  }
-  return avatarURLs;
-}
-
-function generateLocations(min, max, number) {
+function generateRandomArray(min, max, number) {
   var array = [];
   for (var i = 0; i < number; i++) {
     array.push(generateRandomInteger(min, max));
@@ -120,77 +239,10 @@ function generateLocations(min, max, number) {
   return array;
 }
 
-function generateAddresses(number) {
+function generateRandomArrayfromInput(input, number) {
   var array = [];
   for (var i = 0; i < number; i++) {
-    array.push(LOCATIONS_X[i].toString() + ', ' + LOCATIONS_Y[i].toString());
-  }
-  return array;
-}
-
-function generatePrices(number) {
-  var array = [];
-  for (var i = 0; i < number; i++) {
-    array.push(generateRandomInteger(minPrice, maxPrice));
-  }
-  return array;
-}
-
-function generateFeatures(number) {
-  var array = [];
-  for (var i = 0; i < number; i++) {
-    var arrayTemp = [];
-    var featuresNumber = generateRandomInteger(1, FEATURES_VALUES.length);
-    var featuresValuesShuffled = shuffleArray(FEATURES_VALUES);
-    for (var j = 0; j < featuresNumber; j++) {
-      arrayTemp.push(featuresValuesShuffled[j]);
-    }
-    array[i] = arrayTemp;
-  }
-  return array;
-}
-
-function generateAuthorObject(number) {
-  var object = {
-    avatar: AVATARS[number]
-  };
-  return object;
-}
-
-function generateOfferObject(number) {
-  var object = {
-    title: TITLES[number],
-    address: ADDRESSES[number],
-    price: PRICES[number],
-    type: TYPES[generateRandomInteger(0, TYPES.length - 1)],
-    rooms: generateRandomInteger(minRooms, maxRooms),
-    guests: generateRandomInteger(1, maxGuests),
-    checkin: CHECKINS[generateRandomInteger(0, CHECKINS.length - 1)],
-    checkout: CHECKOUTS[generateRandomInteger(0, CHECKOUTS.length - 1)],
-    features: FEATURES[number],
-    description: '',
-    photos: []
-  };
-  return object;
-}
-
-function generateLocationObject(number) {
-  var object = {
-    x: LOCATIONS_X[number],
-    y: LOCATIONS_Y[number]
-  };
-  return object;
-}
-
-function generateOfferedObjects(number) {
-  var array = [];
-  for (var i = 0; i < number; i++) {
-    array[i] =
-        {
-          author: generateAuthorObject(i),
-          offer: generateOfferObject(i),
-          location: generateLocationObject(i)
-        };
+    array.push(input[generateRandomInteger(0, input.length - 1)]);
   }
   return array;
 }
